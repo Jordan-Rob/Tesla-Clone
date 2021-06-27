f67184ab33834e27637eaf0d23976e3ecc21d40a
jordanrob709 <jordanrob709@gmail.com>
2021-06-28 02:51:37 +0300

    added landing section styling

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,66 @@
 import React from 'react';
+import styled from 'styled-components'
 
 const Section = () => {
     return(
-        <div>
-            Section
-        </div>
+        <Wrap>
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+        </Wrap>
     )
 }
+
+const Wrap = styled.div`
+    width:100vw;
+    height:100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-conten: space-between;
+    align-items: center;
+
+`
+
+
+const ItemText = styled.div`
+    padding-top: 10vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height:40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+
+`
+
+const RightButton = styled(LeftButton)
 
 export default Section
